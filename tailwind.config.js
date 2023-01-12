2838a8cfa84244e7055d8821a8d05acb66ea64de
denimarques <101600395+denimarques@users.noreply.github.com>
2023-01-12 20:03:09 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'DM-Sans': ['DM Sans', 'sans-serif'],
+      'Dancing-Script': ['Dancing Script', 'cursive'],
+      'Montserrat': ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
